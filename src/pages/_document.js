7130b6f2d1f8d6d2a2ce268905ab3dd8dd9fede7
rsoraluz <rsoraluz@eltiempo.pe>
2023-08-28 12:48:13 -0500

    anuncio

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
       <script src="https://cdn.jsdelivr.net/npm/in-view@0.6.1/dist/in-view.min.js"></script>
 <script>inView.offset(-200);</script>
 
-<script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
+<script async="async" src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
       </Head>
       <body>
         <Main />
